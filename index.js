37b6a91a10d11dd58faffdb629b0471bc34031df
Anshu Mishra <anshumishra69811@gmail.com>
2026-08-15 21:29:22 +0530

    first js commit

---
+++ index.js
@@ -0,0 +1,31 @@
+// console.log("hello world");
+// console.log(4+2);
+
+// let name = "anshu";
+// let age = 18;
+// console.log(age);
+// console.log(name);
+// console.log(typeof(age));
+// console.log(typeof(name))
+
+// let isLoggedIn = true;
+// console.log(isLoggedIn);
+// console.log(typeof(isLoggedIn));
+
+// let city;
+// console.log(city);
+// console.log(typeof(city));
+
+// let count = null;
+// console.log(count);
+// console.log(typeof(count));
+
+// let name = `anshu`
+// console.log(name , "mishra");
+
+const name = "anshu";
+console.log("2 + 2 = ",4);
+console.log("2 * 5 = ",10);
+console.log("hello my name is ",name);
+
+
