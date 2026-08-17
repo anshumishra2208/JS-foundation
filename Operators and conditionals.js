0531607e3fb9de8e375ea48cac813f829f397e09
Anshu Mishra <anshumishra69811@gmail.com>
2026-08-17 23:03:21 +0530

    basics of js-1

---
+++ Operators and conditionals.js
@@ -0,0 +1,156 @@
+// arithmetic operators
+
+// let num1 = 10;
+// let num2 = 5;
+// console.log(num1 + num2);
+// console.log(num1 - num2);
+// console.log(num1 * num2);
+// console.log(num1 / num2);
+// console.log(num1 % num2);
+// console.log(num1 ** num2);
+
+
+
+// increment decrement operators
+
+// let num1 = 10;
+// num1++;
+// console.log(num1);
+
+// let num1 = 10;
+// console.log(++num1);
+// console.log(num1++);
+// console.log(num1);
+
+
+// assignment operators
+
+// let num1 = 10;
+// num1 += 2;
+// console.log(num1);
+// num1 -= 2;
+// console.log(num1);
+// num1 *= 2;
+// console.log(num1);
+// num1 /= 2;
+// console.log(num1);
+
+
+
+// comparioson operators
+
+// console.log(3>6);
+// console.log(3>=6);
+// console.log(3<6);
+// console.log(3>=6);
+// console.log(6==6);    //loose equality
+// console.log(6===6);   //strict equality where type is also checked and should be same
+// console.log("6"===6);
+// console.log(3!=6);
+
+
+
+// logical operators
+
+// console.log(3>2 && 6>3);
+// console.log(3>2 || 4>8);
+// console.log(3<2 || 4>8);
+// console.log(!3>2);
+
+// const age = 18;
+// let hasAdharCard = true;
+// console.log(age>=18 && hasAdharCard);
+
+
+
+
+// truthy and falsy value  
+
+// console.log(!56);
+// console.log(!0);
+// console.log(!"anshu");
+// console.log(!"");
+// let product = null;
+// console.log(!product);
+
+
+
+
+// Conditionals
+
+// let age = 18;
+// let havePanCard = true;
+// if(age>=18 && havePanCard){
+//     console.log("You Can Avail Our Services");
+// }
+// else{
+//     console.log("Sorry Try Next Time");
+// }
+
+// let isLoggedIn = false;
+// if(isLoggedIn){
+//     console.log("can like and subscrube");
+// }
+// else{
+//     console.log("login first");
+// }
+
+// let temp = 40;
+// if(temp>=25 && temp<=30){
+//     console.log("turn on the ac");
+// }
+// else if(temp<=25){
+//     console.log("turn off the ac");
+// }
+// else{
+//     console.log("let it be as it is")
+// }
+
+// let isLoggedIn = true;
+// let premiumCustomer = true;
+// if(isLoggedIn){
+//     if(premiumCustomer){
+//         console.log("choose your categoties to watch movies");
+//     }
+//     else{
+//         console.log("please subscribe first");
+//     }
+// }
+// else{
+//     console.log("login first");
+// }
+
+
+
+// Switch Case
+
+// let day = "wed";
+// switch(day){
+//     case "mon":
+//     console.log("first day");
+//     break;
+    
+//     case "tue":
+//         console.log("second day");
+//         break;
+
+//         case "wed":
+//         console.log("third day");
+//         break;
+
+//         case "thu":
+//         console.log("fourth day");
+//         break;
+
+//         case "fri":
+//         console.log("fifth day");
+//         break;
+
+//         case "sat":
+//         console.log("sixth day");
+//         break;
+
+//         case "sun":
+//         console.log("seventh day");
+//         break;
+// }
