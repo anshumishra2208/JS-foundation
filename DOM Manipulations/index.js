2be449ad07056ad5cc02f0942e6187f7f2f30fc4
Anshu Mishra <anshumishra69811@gmail.com>
2026-09-08 14:03:12 +0530

    event and event delegation

--- DOM Manipulations/index.js
+++ DOM Manipulations/index.js
@@ -83,4 +83,93 @@
 // let items2 = products;
 // console.log(items2[2]);
 
+let parent = document.querySelector(".parent");
+let btn = document.querySelector("button");
+// btn.addEventListener("click",(e)=>{
+//     console.log("button clicked");
+//     console.log(e.target);
+//     console.log(e.currentTarget);
+// })
+
+let outter = document.querySelector(".outter");
+let inner = document.querySelector(".inner");
+let btn1 = document.querySelector("#btn1");
+// outter.addEventListener("click",(e)=>{
+//     e.stopPropagation();
+//     console.log("outter");
+// })
+// inner.addEventListener("click",(e)=>{
+//     e.stopPropagation();
+//     console.log("inner");
+// })
+// btn1.addEventListener("click",(e)=>{
+//     console.log("button");
+//     e.stopPropagation();
+// })
+
+
+products = [
+    {
+        name : "iphone",
+        price : 4586778,
+        url : "https://m.media-amazon.com/images/I/617O+RkwdPL._AC_UY436_QL65_.jpg"
+    },
+    {
+        name : "laptop",
+        price : 2475,
+        url : "https://m.media-amazon.com/images/I/71MbIVSIhAL._AC_UY436_QL65_.jpg"
+    },
+    {
+        name : "pen",
+        price : 286,
+        url : "https://m.media-amazon.com/images/I/51bqn1--ZbL._AC_UL640_QL65_.jpg"
+    },
+    {
+        name : "cycle",
+        price : 264,
+        url : "https://m.media-amazon.com/images/I/81yW5Z8Yk0L._AC_UY436_QL65_.jpg"
+    }
+]
+
+let parent1 = document.querySelector(".parent1");
+parent1.classList.add("list");
+let dltbtn,card,cart;
+products.forEach((product)=>{
+    card = document.createElement("div")
+    parent1.append(card);
+    card.classList.add("product");
+    card.textContent = `${product.name}`;
+    dltbtn = document.createElement("button");
+    dltbtn.textContent = "Remove item";
+    card.append(dltbtn);
+    cart = document.createElement("button");
+    cart.textContent = "Add to cart";
+    card.append(cart);
+    // dltbtn.addEventListener("click",(e)=>{
+    //     console.log("target",e.target);
+    //     console.log("currentTarget",e.currentTarget);
+        
+    // })
+})
+
+
+parent1.addEventListener("click",(e)=>{
+    // console.log("target",e.target);
+    // console.log("currentTarget",e.currentTarget);
+    // console.log(dltbtn.parentElement);
+    // console.log(e.target.tagName);
+    // console.log(e.target.textContent);
+    
+    if(e.target.tagName==="BUTTON" && e.target.textContent==="Remove item"){
+        // e.target.parentElement.remove();
+        e.target.closest(".product").remove();
+        
+    }
+    if(e.target.tagName==="BUTTON" && e.target.textContent==="Add to cart"){
+        e.target.textContent = "Item Added to cart"
+    }
+})
+
+
+
 
