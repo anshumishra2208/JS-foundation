5d231f35002b3a196bd1089c7c1417629c005dc0
Anshu Mishra <anshumishra69811@gmail.com>
2026-09-08 12:28:58 +0530

    dom and manipulations

---
+++ DOM Manipulations/index.js
@@ -0,0 +1,86 @@
+// let h1 = document.querySelector("h1");
+// h1.textContent = "good morning"
+
+// let h2 = document.querySelector("h2")
+// h2.innerHTML = "<h2>inner html</h2>";
+
+// // console.log(h1.textContent);
+// // console.log(h2.innerHTML);
+
+// let p = document.querySelector("p");
+// // console.log(p.innerText);
+
+// p.setAttribute("style" , "background-color: pink; font-size: 20px;")
+// console.log(p.getAttribute("style"));
+// // p.removeAttribute("style")
+
+// p.style.color = "red";
+// p.style.margin = "10px";
+
+// p.dataset.hello = "hii"
+
+// let h4 = document.querySelector("h4");
+// h4.classList.add("random");
+// h4.classList.remove("random");
+// h4.classList.toggle("random");
+
+// // console.log(h4.classList.contains("random"));
+
+// let body = document.querySelector("body")
+// let h5 = document.createElement("h5")
+// h5.textContent = "this is paragraph2";
+// body.append(h5);
+
+// let div = document.createElement("div");
+// body.append(div);
+// let p1 = document.createElement("p");
+// p1.textContent = "first line";
+// div.append(p1);
+// p1.classList.add("para")
+// let p2 = document.createElement("p");
+// p2.textContent = "this is second line";
+// div.append(p2);
+// p2.classList.add("para");
+// // p2.remove()
+
+
+// products = [
+//     {
+//         name : "iphone",
+//         price : 4586778,
+//         url : "https://m.media-amazon.com/images/I/617O+RkwdPL._AC_UY436_QL65_.jpg"
+//     },
+//     {
+//         name : "laptop",
+//         price : 2475,
+//         url : "https://m.media-amazon.com/images/I/71MbIVSIhAL._AC_UY436_QL65_.jpg"
+//     },
+//     {
+//         name : "pen",
+//         price : 286,
+//         url : "https://m.media-amazon.com/images/I/51bqn1--ZbL._AC_UL640_QL65_.jpg"
+//     },
+//     {
+//         name : "cycle",
+//         price : 264,
+//         url : "https://m.media-amazon.com/images/I/81yW5Z8Yk0L._AC_UY436_QL65_.jpg"
+//     }
+// ]
+
+// let productList = document.createElement("div");
+// body.append(productList);
+// products.forEach((product)=>{
+//     let card = document.createElement("div");
+//     productList.append(card);
+//     card.classList.add("sam")
+//     card.append(`${product.name} - ${product.price}`)
+// })
+
+// let clone = productList.cloneNode(true);
+// let items = clone.children;
+// console.log(items[1]);
+
+// let items2 = products;
+// console.log(items2[2]);
+
+
